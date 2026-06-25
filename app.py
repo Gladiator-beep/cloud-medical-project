fe0e083115fc01619d751b4637c80f4f714c2ba6
Shon Eliav <shoneliav10@gmail.com>
2026-06-25 16:02:59 +0300

    Configure app port for Render

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ def generated_pdf(filename):
 with app.app_context():
     db.create_all()
 
+import os
+
 if __name__ == "__main__":
     print("Starting Flask server...")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
